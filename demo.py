9772cfc7e93ddd3a690af038936a30d7ee315ba8
RamiF13 <115772089+RamiF13@users.noreply.github.com>
2026-05-06 22:32:03 -0300

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+from PIL import Image
+import streamlit as st
+
+modelo = tf.keras.models.load_model("modelo_ajedrez.keras")
+nombres = ['Alfil blanco', 'Alfil negro', 'Caballo blanco', 'Caballo negro', 'Dama blanca', 'Dama negra', 'Peon blanco', 'Peon negro', 'Rey blanco', 'Rey negro', 'Torre blanca', 'Torre negra']
+
+st.title("Clasificador de piezas de ajedrez")
+
+archivo = st.file_uploader("Sube una imagen")
+
+if archivo is not None: 
+    img_original = Image.open(archivo)
+    img = np.array(img_original)
+    img = cv2.resize(img,(224,224))
+    img = img * 1/255
+    img = np.expand_dims(img,axis=(0))
+    prediccion = modelo.predict(img)
+    clase = np.argmax(prediccion)
+    nombre = nombres[clase]
+    confianza = prediccion[0][clase]
+    st.image(img_original)
+    st.write(f'**{nombre}**,- Confianza: {round(float(confianza)*100,1)}')
